54a1e01adb74ccc9fcb2d3ea5632c6f35d1a78e7
Matthias Ableidinger <ableimat@gmx.at>
2017-09-02 12:09:56 +0200

    no host checking

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -3,8 +3,8 @@
 echo "$key" | base64 > key
 
 #TODO collect war to deploy_target
-scp -P 22222 -i id_rsa -r deploy_target travis@188.68.35.134:deploy_target
+scp -P 22222 -i id_rsa -r -o StrictHostKeyChecking=no deploy_target travis@188.68.35.134:deploy_target
 
-ssh travis@dev.slothyx.com -p 22222 -i key "deploy_target/deploy_on_server.sh"
+ssh travis@dev.slothyx.com -p 22222 -i key -o StrictHostKeyChecking=no "deploy_target/deploy_on_server.sh"
 
 rm -f key
