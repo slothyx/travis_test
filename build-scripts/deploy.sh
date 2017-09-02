44a501dffd292f06774b84ac9eba570b3b94ede6
Matthias Ableidinger <ableimat@gmx.at>
2017-09-02 12:05:25 +0200

    deploy on server

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-cd backend
-if [ -f "target/travistest-1.0-SNAPSHOT.jar" ]
-then
-  echo "file is here"
-else
-  echo "file is gone :("
-fi
-echo "deploying all the shit"
+echo "$key" | base64 > key
+
+#TODO collect war to deploy_target
+scp -P 22222 -i id_rsa -r deploy_target travis@188.68.35.134:deploy_target
+
+ssh travis@dev.slothyx.com -p 22222 -i key "deploy_target/deploy_on_server.sh"
+
+rm -f key
