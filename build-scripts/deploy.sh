adaf1146c41b853cca88d19f4905e7f5c8734e77
Matthias Ableidinger <ableimat@gmx.at>
2017-09-02 12:33:17 +0200

    works!

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -2,13 +2,11 @@
 
 set -ev
 
-echo "$key"
 echo "$key" | base64 -d > key
-cat key
 chmod 600 key
 
 #TODO collect war to deploy_target
-scp -P 22222 -i key -r -o StrictHostKeyChecking=no deploy_target travis@188.68.35.134:deploy_target
+scp -P 22222 -i key -r -o StrictHostKeyChecking=no deploy_target travis@dev.slothyx.com:deploy_target
 
 ssh travis@dev.slothyx.com -p 22222 -i key -o StrictHostKeyChecking=no "deploy_target/deploy_on_server.sh"
 
